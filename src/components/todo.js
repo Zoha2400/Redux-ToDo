abb191abc6becc8e860d1769030545259c876c66
Zohir <76750755+Zoha2400@users.noreply.github.com>
2023-11-05 20:08:30 +0000

    Update todo.js and todo.scss

--- src/components/todo.js
+++ src/components/todo.js
@@ -3,10 +3,13 @@ import "../styles/todo.scss";
 export default function ToDo() {
   return (
     <div className="ToDo-wrap">
-      <p className="message">do homework</p>
+      <p className="message">
+        <p className="num">1</p>
+        <p className="text">Do Homework</p>
+      </p>
       <div className="buttons">
-        <div className="done btn">D</div>
-        <div className="remove btn">X</div>
+        <div className="done btn"></div>
+        <div className="remove btn"></div>
       </div>
     </div>
   );
