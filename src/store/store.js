be0b1a1905d99ea7a1412308f34ab8dbf1b2d81a
Zohir <76750755+Zoha2400@users.noreply.github.com>
2023-11-07 00:13:25 +0000

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,12 @@
 import { createStore } from "redux";
+
+const toDo = (state = [], active) => {
+  if (active.type === "ADD") {
+    state.concat(active.todo);
+  } else if (active.type === "REMOVE") {
+    state.splice(active.todo);
+  } else if (active.type === "DONE") {
+  }
+};
+
+const store = createStore(toDo);
